cb766499ab6b941d5934711c5a6bc7d9263c59d5
arvid.nicolaas <arvid.nicolaas@philips.com>
2021-06-07 18:31:53 +0200

    fix(list): reorder imports to fix codesandbox import

--- packages/list/src/list-custom.ts
+++ packages/list/src/list-custom.ts
@@ -1,30 +1,33 @@
+// only circular types
 export * from './implementation/block';
 export * from './implementation/nonempty-base';
 export * from './implementation/empty';
-
-// generic implementation
 export * from './implementation/nonleaf/interface';
 export * from './implementation/tree/interface';
-
-export * from './implementation/leaf/leaf-block';
-export * from './implementation/leaf/reversed-leaf-block';
-export * from './implementation/leaf/leaf-tree';
-export * from './implementation/nonleaf/nonleaf-block';
-export * from './implementation/nonleaf/nonleaf-tree';
-export * from './implementation/tree/operations';
-
-// generic builder
 export * from './builder/builder-base';
-export * from './builder/gen-builder';
-export * from './builder/block-builder';
 export * from './builder/leaf/leaf-builder';
 export * from './builder/leaf/block-builder';
 
-export * from './builder/tree/tree-builder-base';
-export * from './builder/nonleaf/builder';
-export * from './builder/leaf/tree-builder';
-export * from './builder/nonleaf/block-builder';
-export * from './builder/nonleaf/tree-builder';
+// circular dependencies
+export * from './implementation/leaf/leaf-block';
+export * from './implementation/leaf/reversed-leaf-block';
+
+export * from './implementation/tree/operations';
+
+// mutual dependencies
+export * from './implementation/nonleaf/nonleaf-block';
+export * from './implementation/nonleaf/nonleaf-tree';
 
 // other
+export * from './implementation/leaf/leaf-tree';
+
+export * from './builder/gen-builder';
+export * from './builder/block-builder';
+export * from './builder/nonleaf/block-builder';
+export * from './builder/nonleaf/nonleaf-builder';
+
+export * from './builder/tree/tree-builder-base';
+export * from './builder/leaf/tree-builder';
+export * from './builder/nonleaf/tree-builder';
+
 export * from './context';
