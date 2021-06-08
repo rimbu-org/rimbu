f2698c19102e500057e22c9c3f5b4135e0bb1189
arvid.nicolaas <arvid.nicolaas@philips.com>
2021-06-08 17:32:16 +0200

    fix(list): rework dependencies

--- packages/list/src/list-custom.ts
+++ packages/list/src/list-custom.ts
@@ -11,15 +11,13 @@ export * from './builder/gen-builder';
 export * from './builder/leaf/block-builder';
 export * from './builder/leaf/leaf-builder';
 export * from './builder/nonleaf/nonleaf-builder';
-export * from './builder/tree/tree-builder-base';
 
 // circular dependencies
 export * from './implementation/leaf/non-empty';
+export * from './builder/tree/tree-builder';
 
 export * from './implementation/nonleaf/nonleaf-tree';
 
-export * from './builder/leaf/tree-builder';
 export * from './builder/nonleaf/block-builder';
-export * from './builder/nonleaf/tree-builder';
 
 export * from './context';
