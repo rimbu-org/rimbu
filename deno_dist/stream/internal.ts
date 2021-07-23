adfbe198ed50f9ea5e754a55cce8e1fc628fa649
arvid.nicolaas <arvid.nicolaas@philips.com>
2021-07-23 18:32:50 +0200

    fix(stream): reorder imports to avoid CodeSandbox error

--- deno_dist/stream/internal.ts
+++ deno_dist/stream/internal.ts
@@ -6,11 +6,11 @@ export * from './stream/interface.ts';
 export * from './stream/streamable.ts';
 export * from './stream/stream-source.ts';
 
-export * from './async-stream/utils.ts';
-export * as AsyncStreamCustom from './async-stream/async-stream-custom.ts';
-
-export * from './async-stream/interface.ts';
 export * from './async-stream/async-fast-iterator.ts';
 export * from './async-stream/async-fast-iterable.ts';
 export * from './async-stream/async-streamable.ts';
+
+export * as AsyncStreamCustom from './async-stream/async-stream-custom.ts';
+
+export * from './async-stream/interface.ts';
 export * from './async-stream/async-stream-source.ts';
--- packages/stream/src/internal.ts
+++ packages/stream/src/internal.ts
@@ -6,11 +6,11 @@ export * from './stream/interface';
 export * from './stream/streamable';
 export * from './stream/stream-source';
 
-export * from './async-stream/utils';
-export * as AsyncStreamCustom from './async-stream/async-stream-custom';
-
-export * from './async-stream/interface';
 export * from './async-stream/async-fast-iterator';
 export * from './async-stream/async-fast-iterable';
 export * from './async-stream/async-streamable';
+
+export * as AsyncStreamCustom from './async-stream/async-stream-custom';
+
+export * from './async-stream/interface';
 export * from './async-stream/async-stream-source';
