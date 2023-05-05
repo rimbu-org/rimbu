c6fafcc020901c5bbaac33b1dc9477752bffb05b
Arvid Nicolaas <arvid.nicolaas@ns.nl>
2023-05-05 10:38:15 +0200

    refactor(deep): use built-in readonly collections instead of custom

--- deno_dist/typical/index.ts
+++
@@ -1,6 +0,0 @@
-import type * as U from './utils.ts';
-import type * as Str from './str.ts';
-import type * as StrNum from './strnum.ts';
-import type * as Num from './num.ts';
-
-export type { U, Str, StrNum, Num };
