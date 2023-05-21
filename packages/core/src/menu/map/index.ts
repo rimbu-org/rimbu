b063025b7b2ff1ecb3095e44e928721bc22a77f4
Gianluca Costa <gianluca@gianlucacosta.info>
2023-05-21 03:23:20 +0200

    Register "proximity" into the "core" package

--- packages/core/src/menu/map/index.ts
+++ packages/core/src/menu/map/index.ts
@@ -1,4 +1,5 @@
 export { HashMap as Hashed } from '@rimbu/hashed/map';
 export { SortedMap as Sorted } from '@rimbu/sorted/map';
+export { ProximityMap as Proximity } from '@rimbu/proximity/map';
 
 export * as Ordered from './ordered';
