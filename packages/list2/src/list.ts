4debca544fc1f4ef10ed8f8f786c12d8967c784b
Arvid Nicolaas <arvid.nicolaas@ns.nl>
2026-08-02 10:33:51 +0200

    refactor: add capabilities

--- packages/list2/src/list.ts
+++ packages/list2/src/list.ts
@@ -26,7 +26,9 @@ export type OpWithChangeResult<
 	| OpWithResult<ColWithoutResult, ResultWithoutHasResult, false>
 	| OpWithResult<ColWithResult, ResultWithHasResult, true>;
 
-export interface List<T> extends IndexedCollection<T>, List.Capabilities<T> {
+export interface List<T>
+	extends IndexedCollection<T>,
+		List.Advanced.Capabilities<T> {
 	readonly context: List.Context<T>;
 }
 
@@ -50,56 +52,54 @@ export declare namespace List {
 		appendAll(elements: StreamSource<T>): void;
 	}
 
-	export interface Capabilities<T>
-		extends Collection.Capability.WithFilter<T>,
-			IndexedCollection.Capability.WithOrderEditable<T>,
-			IndexedCollection.Capability.WithMap<T>,
-			List.WithUpdateAt<T>,
-			List.WithConcat<T>,
-			List.WithReversed<T> {
-		readonly context: List.Context<T>;
+	export namespace Capability {
+		export interface WithUpdateAt<E> extends IndexedCollection<E> {
+			setAt(index: number, element: E): this['context']['__types']['_SELF'];
+			setAtAndReturn(
+				index: number,
+				element: E,
+			): OpWithChangeResult<
+				this['context']['__types']['_SELF'],
+				undefined,
+				E,
+				this['context']['__types']['_NON_EMPTY']
+			>;
+			updateAt(
+				index: number,
+				f: (element: E) => E,
+			): this['context']['__types']['_SELF'];
+			updateAtAndReturn(
+				index: number,
+				f: (element: E) => E,
+			): OpWithChangeResult<
+				this['context']['__types']['_SELF'],
+				[previous: undefined, current: undefined],
+				[previous: E, current: E],
+				this['context']['__types']['_NON_EMPTY']
+			>;
+		}
+
+		export interface WithConcat<E> extends IndexedCollection<E> {
+			concat(
+				...sources: ArrayNonEmpty<StreamSource.NonEmpty<E>>
+			): this['context']['__types']['_NON_EMPTY'];
+			concat(
+				...sources: ArrayNonEmpty<StreamSource<E>>
+			): this['context']['__types']['_SELF'];
+		}
+
+		export interface WithReversed<E> extends IndexedCollection<E> {
+			reversed(): this['context']['__types']['_SELF'];
+		}
 	}
 
-	export interface WithUpdateAt<E> extends IndexedCollection<E> {
-		setAt(index: number, element: E): this['context']['__types']['_SELF'];
-		setAtAndReturn(
-			index: number,
-			element: E,
-		): OpWithChangeResult<
-			this['context']['__types']['_SELF'],
-			undefined,
-			E,
-			this['context']['__types']['_NON_EMPTY']
-		>;
-		updateAt(
-			index: number,
-			f: (element: E) => E,
-		): this['context']['__types']['_SELF'];
-		updateAtAndReturn(
-			index: number,
-			f: (element: E) => E,
-		): OpWithChangeResult<
-			this['context']['__types']['_SELF'],
-			[previous: undefined, current: undefined],
-			[previous: E, current: E],
-			this['context']['__types']['_NON_EMPTY']
-		>;
-	}
-
-	export interface WithConcat<E> extends IndexedCollection<E> {
-		concat(
-			...sources: ArrayNonEmpty<StreamSource.NonEmpty<E>>
-		): this['context']['__types']['_NON_EMPTY'];
-		concat(
-			...sources: ArrayNonEmpty<StreamSource<E>>
-		): this['context']['__types']['_SELF'];
-	}
-
-	export interface WithReversed<E> extends IndexedCollection<E> {
-		reversed(): this['context']['__types']['_SELF'];
-	}
-
-	export interface Context<T, IsNonEmpty extends boolean = boolean> {
+	export interface Context<T, IsNonEmpty extends boolean = boolean>
+		extends List.Advanced.Factory<
+			T,
+			IsNonEmpty extends true
+				? List.Advanced.TypesNonEmpty<T>
+				: List.Advanced.Types<T>
+		> {
 		readonly blockSizeBits: number;
 
 		__types: IsNonEmpty extends true
@@ -128,19 +128,45 @@ export declare namespace List {
 		})['_NEW_TYPES']['_BUILDER'];
 	}
 
-	export type Factory = Pick<
-		List.Context<any>,
-		'empty' | 'of' | 'from' | 'builder' | 'createContext'
-	>;
-
-	interface Concat<T, LN, LNE, IsNonEmpty extends boolean = boolean> {
-		concat(...sources: ArrayNonEmpty<StreamSource.NonEmpty<T>>): LNE;
-		concat(
-			...sources: ArrayNonEmpty<StreamSource<T>>
-		): IsNonEmpty extends true ? LNE : LN;
-	}
-
 	export namespace Advanced {
+		export interface Capabilities<T>
+			extends Collection.Capability.WithFilter<T>,
+				IndexedCollection.Capability.WithOrderEditable<T>,
+				IndexedCollection.Capability.WithMap<T>,
+				List.Capability.WithUpdateAt<T>,
+				List.Capability.WithConcat<T>,
+				List.Capability.WithReversed<T> {
+			readonly context: List.Context<T>;
+		}
+
+		export interface Factory<
+			T = any,
+			Tp extends List.Advanced.Types<T> | List.Advanced.TypesNonEmpty<T> =
+				| List.Advanced.Types<T>
+				| List.Advanced.TypesNonEmpty<T>,
+		> {
+			createContext(options: { blockSizeBits?: number }): List.Context<T>;
+
+			empty<T extends Tp['_UPPER_E']>(): (Tp & {
+				_NEW_E: T;
+			})['_NEW_TYPES']['_NORMAL'];
+
+			of<T extends Tp['_UPPER_E']>(
+				...elements: ArrayNonEmpty<T>
+			): (Tp & { _NEW_E: T })['_NEW_TYPES']['_NON_EMPTY'];
+
+			from<T extends Tp['_UPPER_E']>(
+				...sources: ArrayNonEmpty<StreamSource.NonEmpty<T>>
+			): (Tp & { _NEW_E: T })['_NEW_TYPES']['_NON_EMPTY'];
+			from<T extends Tp['_UPPER_E']>(
+				...sources: ArrayNonEmpty<StreamSource<T>>
+			): (Tp & { _NEW_E: T })['_NEW_TYPES']['_NORMAL'];
+
+			builder<T extends Tp['_UPPER_E']>(): (Tp & {
+				_NEW_E: T;
+			})['_NEW_TYPES']['_BUILDER'];
+		}
+
 		export interface Types<T> extends IndexedCollection.Advanced.Types<T> {
 			_NORMAL: List<T>;
 			_NON_EMPTY: List.NonEmpty<T>;
@@ -158,7 +184,7 @@ export declare namespace List {
 	}
 }
 
-export const List: List.Factory = createListContextModule({
+export const List: List.Advanced.Factory = createListContextModule({
 	blockSizeBits: 5,
 	childrenOps: new ArrayOuterChildrenOps() as ChildrenOps,
 });
